cd9d66f0d00758ae1338ca6fbdd84e2f3769f5b1
day-dreamerTo <s964412214@163.com>
2017-04-14 14:24:00 +0800

    简单测试

---
+++ dist/js/nav.js
@@ -0,0 +1,6 @@
+/**
+ * Created by admin on 17/4/14.
+ */
+mui('body').on('tap','.nav li',function(){
+    alert(1);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -8,42 +8,51 @@ var gutil = require('gulp-util')
 var data = require('gulp-data')
 var browserSync = require('browser-sync')
 var ejs = require('gulp-ejs')
-
+var rename = require('gulp-rename');
 
 var tplDir = './tpl'  // 模版目录
-var distDir = './temphtml'      // 生成目录
+var distDir = './html'      // 生成目录
 
 // 模版合并
 gulp.task('ejs', function(){
     gulp.src(tplDir + '/**/*.html')
-        /*.pipe(data(function (file) {
+        .pipe(data(function (file) {
             var filePath = file.path;
             gutil.log(filePath); // /Users/admin/Desktop/gulp-ejs-demo/templates/index.html
             // global.json 全局数据，页面中直接通过属性名调用
-            return Object.assign(JSON.parse(fs.readFileSync(tplDir + '/global.json')), {
+            var obj = Object.assign(JSON.parse(fs.readFileSync('./global.json')), {
                 // local: 每个页面对应的数据，页面中通过 local.属性 调用
                 local: JSON.parse(fs.readFileSync( path.join(path.dirname(filePath), path.basename(filePath, '.html') + '.json')))
-            })
-        }))*/
+            });
+            return obj;
+        }))
         .pipe(ejs().on('error', function(err) {
             gutil.log(err);
             this.emit('end');
         }))
+        .pipe(rename({dirname: ''}))
         .pipe(gulp.dest(distDir));
 });
-gulp.task('copyhtml',function(){
-    gulp.src(distDir + '/**/*.html')
-        .pipe(gulp.dest('./html/'));
+gulp.task('copy',function(){
+    gulp.src([tplDir + '/**/*.css'])
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest('./dist/css/'));
+    gulp.src([tplDir + '/**/*.js'])
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest('./dist/js/'));
+    gulp.src([tplDir + '/**/*.png'])
+        .pipe(rename({dirname: ''}))
+        .pipe(gulp.dest('./dist/images/'));
 });
-
-gulp.task('ejs-watch', ['ejs','copyhtml'], browserSync.reload);
+gulp.task('ejs-watch',['ejs','copy'], browserSync.reload);
 
 // 开发服务
 gulp.task('dev', function() {
 
     browserSync.init({
         server: {
-            baseDir: './html'
+            baseDir: './', // 注意这里的路径
+            index: './html/index.html'
         },
         reloadDebounce: 0
     });
